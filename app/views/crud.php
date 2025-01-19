6ee06a8852f48988af12bf5c02aee57b3ecaa7f6
EdouardoRabe <edouardorabendrazana@gmail.com>
2025-01-19 15:43:52 +0300

    feat: add adminController , modif function generateTableau in generaliserModel, add routes, add crud .php

---
+++ app/views/crud.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/crud.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-free-6.6.0-web/css/all.min.css">
+    <title>CRUD</title>
+</head>
+<body>
+    <?= $table?>
+</body>
+</html>
