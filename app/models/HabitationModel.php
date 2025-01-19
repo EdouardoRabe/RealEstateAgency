4d483be91a248e7ece041380305071a78709cd81
EdouardoRabe <edouardorabendrazana@gmail.com>
2025-01-19 13:11:43 +0300

    feat: add detail.php, detail.css, DetailController and function generateHabitationImagesHtml at HabitationModel

--- app/models/HabitationModel.php
+++ app/models/HabitationModel.php
@@ -72,6 +72,36 @@ class HabitationModel
         }
     }
 
+    public function getHabitationById($id_habitation)
+    {
+        try {
+            $query = "SELECT h.*, t.name AS type_name, i.image_path
+                    FROM agence_habitations h
+                    LEFT JOIN agence_habitation_type t ON h.type = t.id_habitation_type
+                    LEFT JOIN agence_habitation_images i ON h.id_habitation = i.id_habitation
+                    WHERE h.id_habitation = :id_habitation";
+            $params = [':id_habitation' => $id_habitation];
+            $stmt = $this->bdd->prepare($query);
+            $stmt->execute($params);
+            $habitacion = $stmt->fetchAll();
+            
+            if (empty($habitacion)) {
+                return null;
+            }
+            $result = $habitacion[0];
+            $result['images'] = [];
+            foreach ($habitacion as $hab) {
+                if (!empty($hab['image_path'])) {
+                    $result['images'][] = $hab['image_path'];
+                }
+            }
+            return $result;
+        } catch (Exception $e) {
+            throw new Exception("Erreur lors de la récupération de l'habitation : " . $e->getMessage());
+        }
+    }
+
+
 
 
 
@@ -88,7 +118,7 @@ class HabitationModel
                 <a href="' . htmlspecialchars($linkPath) . '?id_habitation=' . urlencode($habitation['id_habitation']) . '" class="card-link">
                     <div class="card">
                         <div class="card-image">
-                            <img src="' . $image . '" alt="Habitation Image" class="image">
+                            <img src="assets/img/' . $image . '" alt="Habitation Image" class="image">
                         </div>
                         <div class="card-content">
                             <h3 class="card-title">' . htmlspecialchars($habitation['type_name']) . '</h3>
@@ -103,6 +133,46 @@ class HabitationModel
 
         return $cards;
     }
+
+    public function generateHabitationImagesHtml($habitation, $defaultImagePath)
+    {
+        try {
+            if ($habitation === null) {
+                return '<div class="no-results">Habitation non trouvée.</div>';
+            }
+            $images = $habitation['images'];
+            if (empty($images)) {
+                $images = [$defaultImagePath]; 
+            }
+            $html = '<div class="habitation-images">';
+            $html .= '<div class="large-image">
+                        <img src="assets/img/' . htmlspecialchars($images[0]) . '" alt="Habitation Image" class="image-large">
+                    </div>';
+            $html .= '<div class="image-grid">';
+            for ($i = 1; $i < count($images); $i++) {
+                $html .= '<div class="image-item">
+                            <img src="assets/img/' . htmlspecialchars($images[$i]) . '" alt="Habitation Image' . ($i + 1) . '" class="image-small">
+                        </div>';
+            }
+
+            while (count($images) < 5) {
+                $html .= '<div class="image-item">
+                            <img src="assets/img/' . htmlspecialchars($defaultImagePath) . '" alt="Habitation Image" class="image-small">
+                        </div>';
+                $images[] = $defaultImagePath; 
+            }
+
+            $html .= '</div>'; 
+
+            $html .= '</div>'; 
+
+            return $html;
+
+        } catch (Exception $e) {
+            throw new Exception("Erreur lors de la récupération des images : " . $e->getMessage());
+        }
+    }
+
 
 
 
